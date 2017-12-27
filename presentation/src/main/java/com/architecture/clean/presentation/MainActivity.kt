a3d2e544e8799b5f1e41f89ede893d1a8973b832
maxim <maxim.bircu@jivygroup.com>
2017-12-27 12:07:17 +0200

    Init the project and started working on build system

---
+++ presentation/src/main/java/com/architecture/clean/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.architecture.clean.presentation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.architecture.clean.android_clean_architecture.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
